91c624e3459dba6c6dd0e2b0cf4bb0352b4a8137
Rather <supertyrian@gmail.com>
2014-05-26 19:35:33 -0700

    adding force graph display of connected peers

--- obelisk.js
+++ obelisk.js
@@ -8,16 +8,79 @@
 // will need to listen on multiple ports:
 //  port at: ui
 //  port lots: peer conxn
-define('obelisk', ['planet'], function(planet) {
+define('obelisk', ['planet', 'd3'], function(Planet, d3) {
 	var corpus = ['courier', 'messenger', 'carrier', 'postmaster', 'notetaker', 'deliverer', 'budare'];
 
-	var Obelisk = function() {
-		this.planet = new planet.Planet(corpus);
-		//this.peerGraph = d3.newGraph('forceDirected');
-		//this.planet.on('orbit', function() {
-			//this.peerGraph.addNode();
-		//});
+	var tick = function() {
+		this.graphNodes
+			.attr('cx', function(d) { return d.x; })
+			.attr('cy', function(d) { return d.y; });
+		this.graphLinks
+			.attr('x1', function(d) { return d.source.x; })
+			.attr('x2', function(d) { return d.target.x; })
+			.attr('y1', function(d) { return d.source.y; })
+			.attr('y2', function(d) { return d.target.y; });
+	};
 
+	var mousedown = function() {
+
+	};
+
+	var Obelisk = function() {
+		var self = this;
+		this.planet = new Planet(corpus);
+		var width = 500;
+		var height = 500;
+		var svg = d3.select('body').append('svg')
+			.attr('width', width)
+			.attr('height', height)
+			.on('mousedown', mousedown);
+		this.graphNodes = svg.selectAll('.node');
+		this.graphLinks = svg.selectAll('.link');
+
+		this.peerGraph = d3.layout.force()
+			.size([width, height])
+			.linkDistance(20)
+    		.gravity(.05)
+			.charge(-60)
+			.on('tick', tick.bind(this))
+			.start();
+		var nodes = this.peerGraph.nodes();
+		var links = this.peerGraph.links();
+		var nodesMap = {};
+
+		this.planet.on('msg', function(msg) {
+			var path = msg.path;
+			var newNodes = path.map(function(nodeName) { return {meta: { name: nodeName } }; });
+			var nodeData = d3.nest()
+				.key(function(d) { return d.meta.name; })
+				.map(d3.merge([nodes, newNodes]), d3.map).values().map(function(a) { return a[0]; });
+
+			self.graphNodes = self.graphNodes.data(nodeData);
+			self.graphNodes
+				.enter().append('circle')
+				.attr('class', 'node')
+				.attr('r', 5)
+				.each(function(newNode) { nodes.push(newNode); nodesMap[newNode.meta.name] = newNode; })
+				.call(self.peerGraph.drag);
+			self.graphNodes
+				.exit().remove();
+
+			var newLinks = d3.pairs(path).map(function(pair) { return { source:nodesMap[pair[0]], target:nodesMap[pair[1]] }; });
+			var linkData = d3.nest()
+				.key(function(d) { return d.source.meta.name + ',' + d.target.meta.name; })
+				.map(d3.merge([links, newLinks]), d3.map).values().map(function(a) { return a[0]; });
+
+			self.graphLinks = self.graphLinks.data(linkData);
+			self.graphLinks
+				.enter().insert('line', '.node')
+				.attr('class', 'link')
+				.each(function(newLink) { links.push(newLink); });
+			self.graphLinks
+				.exit().remove();
+
+			self.peerGraph.start();
+		});
 	};
 
 	Obelisk.prototype.broadcast = function(msg) {
--- planet.js
+++ planet.js
@@ -17,54 +17,84 @@
 /*
 {
 	name: "et cetera",
-	uri: "1.2.3.4:1234/hello",
-	lastSeen: Date.now()
+	uri: "jkhlgfdabnkvcwek",
+	lastSeen: Date.now(),
+	connections: [fgsfds, wjkafnkw, doop]
 }
 */
-define('planet', [], function() {
+define('planet', ['EventEmitter'], function(EventEmitter) {
 
 	var Planet = function(corpus) {
 		this.corpus = corpus;
 		this.chooseName(this.corpus);
 		console.log('somebody: initializing... i am... ' + this.name);
-		this.peers = {};
-		this.hosts = {};
 		this.mainReady = false;
 		this.alphaReady = false;
+		this.messageHistory = [];
+		this.connectedPeers = [];
 
 		this.inititateConnection();
 	};
+	Planet.prototype = Object.create(EventEmitter.prototype);
 
+	// Creates main local peer and connects that peer to alpha peer
 	Planet.prototype.inititateConnection = function() {
 		var self = this;
+
+		// Returns a function that sends data to a specified connection
 		var send = function(data) {
 			return function(conn) {
+				// TODO: don't send data to the sender of the message
 				conn.send(data);
 			}
 		};
+
+		// Attempt to create the alpha node
+		// If this succeeds, setup message routing
+		// If this fails, alpha already exists somewhere else
+		// Either way, main will be able to connect to alpha
 		var createAlpha = function(callback) {
 			var alpha = new Peer('alpha', {key:'vm4nrce6ilfpqfr'});
 			alpha.on('open', function() {
 				console.log('created alpha');
 				var alphaConns = [];
+
+				// Alpha automatically redirects all messages to all other connected peers
 				alpha.on('connection', function(conn) {
-					self.log('alpha: new connection');
+					self.log('alpha: new connection:', conn);
 					alphaConns.push(conn);
 					conn.on('data', function(data) {
-						self.log('alpha:', data);
+						//self.log('alpha:', data);
+						data.path.push('alpha');
 						alphaConns.forEach(send(data));
 					});
+					// handle connection close
 				});
-				callback();
+
+				// Succeeded creating alpha node
+				if(callback) { 
+					var x = callback;
+					callback = null;
+					x();
+				}
 			});
 			alpha.on('error', function(err) {
-				self.log('alpha error:', err);
-				callback();
+				// Probably failed to create alpha node
+				if(callback) { 
+					var x = callback;
+					callback = null;
+					x();
+				}
+				// TODO decide if need to try recreate
 			});
 			alpha.on('close', function() {
 				self.log('alpha closed');
+				// TODO attempt to recreate
 			});
 		};
+
+		// Create the main peer
+		// This one will directly connect to other peers found by talking to alpha
 		self.main = new Peer({key:'vm4nrce6ilfpqfr'});
 		self.main.on('open', function(id) {
 			console.log('main created: ' + id);
@@ -74,19 +104,26 @@ define('planet', [], function() {
 			self.mainReady = true;
 			createAlpha(function() {
 				self.log('connecting main to alpha');
-				self.alpha = self.main.connect('alpha', {metadata:{name:self.name, id:self.main.id}});
-				self.alpha.on('open', function() {
+				var meta = {name:self.name, id:self.main.id};
+				self.listen = self.main.connect('alpha', {metadata:meta});
+				//self.listen.metadata = meta;
+				self.listen.on('open', function() {
 					self.log('connected main to alpha');
 					self.alphaReady = true;
 					self.broadcast('hello from ' + self.name);
+					self.emit('orbit', 'alpha');
 				});
-				self.alpha.on('data', function(data) {
-					self.log('alpha: from ' + data.from + ': ' + data.msg);
+				self.listen.on('data', function(data) {
+					// check for malformed packet
+					if(!data.msg || !data.from || !data.to || !data.path) return;
+					data.path.push(self.name);
+
+					self.message(data);
 				})
-				self.alpha.on('close', function() {
+				self.listen.on('close', function() {
 					self.alphaReady = false;
 				});
-				self.alpha.on('error', function() {
+				self.listen.on('error', function() {
 					self.alphaReady = false;
 				});
 			});
@@ -101,8 +138,18 @@ define('planet', [], function() {
 
 	Planet.prototype.broadcast = function(data) {
 		if(this.mainReady && this.alphaReady) {
-			this.alpha.send({msg: data, from: this.name});
+			// replace with peers.forEach(send)
+			// currently only talks to the alpha node
+			this.listen.send({msg: data, from: this.name, to:'broadcast', path:[this.name]});
+		} else {
+			this.log('not ready to send messages');
 		}
+	};
+
+	Planet.prototype.message = function(msg) {
+		this.messageHistory.push(msg);
+		this.emit('msg', msg);
+		this.log(msg.from + ': ' + msg.msg, msg.path);
 	};
 
 	Planet.prototype.ready = function() {
@@ -120,7 +167,13 @@ define('planet', [], function() {
 		this.name = corpus[pos] + '_' + Math.floor(Math.random() * 10000).toString(Math.floor(Math.random() * 30+2)) ;
 	};
 
-	return {
-		'Planet': Planet
+	Planet.prototype.getPeers = function() {
+		return this.connectedPeers;
 	};
+
+	Planet.prototype.getMessages = function() {
+		return this.messageHistory;
+	};
+
+	return Planet;
 });
